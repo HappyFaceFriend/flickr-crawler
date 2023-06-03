de9b8379693b759ff91d8d82ca2305254aef903a
HappyFaceFriend <yjl980730@gmail.com>
2023-06-04 08:48:40 +0900

    now skips already downloaded photos before querying getSize

--- crawler.py
+++ crawler.py
@@ -64,33 +64,37 @@ for page_num in range(PAGE_START, PAGE_END + 1):
         #API reqeust for getSizes
         photo_id = searched_record["id"]
         unique_name = photo_id + searched_record["owner"]
-        photo_data = flickr_api.get_photoURLs(photo_id)
-        time.sleep(1) #sleep because 3600 request per hour limit
 
-        #Download photo if avilable
+        #Skip if already downloaded
         if unique_name in already_downloaded_ids:
             skipped_count += 1
             total_skipped_count += 1
             continue
-        if True:# photo_data["candownload"]:
-            #Set desired size
-            download_size = "Original"
-            for size in size_preference:
-                if size in photo_data["sizes"].keys():
-                    download_size = size
-                    break
-            image_url = photo_data["sizes"][download_size]["source"]
-            #Download image
-            file_utils.download_and_save_image(image_url, OUTPUT_IMAGE_DIR + unique_name + ".jpg")
-            downloaded_count += 1
-            total_downloaded_count += 1
+        
+        photo_data = flickr_api.get_photoURLs(photo_id)
+        time.sleep(1) #sleep because 3600 request per hour limit
 
-            #Write to csv file
-            searched_record["id"] = unique_name
-            with open(OUTPUT_CSV_PATH, mode='a', encoding = 'utf-8', newline='') as file:
-                writer = csv.DictWriter(file, fieldnames = data_keys)
-                line = {key: searched_record[key] for key in data_keys}
-                writer.writerow(line)
+        #Set desired size
+        download_size = "Original"
+        for size in size_preference:
+            if size in photo_data["sizes"].keys():
+                download_size = size
+                break
+        if download_size == "Original" and not searched_record["candownload"]:
+            download_size = photo_data["sizes"].keys()[-1]
+        image_url = photo_data["sizes"][download_size]["source"]
+        
+        #Download image
+        file_utils.download_and_save_image(image_url, OUTPUT_IMAGE_DIR + unique_name + ".jpg")
+        downloaded_count += 1
+        total_downloaded_count += 1
+
+        #Write to csv file
+        searched_record["id"] = unique_name
+        with open(OUTPUT_CSV_PATH, mode='a', encoding = 'utf-8', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames = data_keys)
+            line = {key: searched_record[key] for key in data_keys}
+            writer.writerow(line)
 
     print(f"Downloaded {downloaded_count}, Skipped {skipped_count} out of {len(search_results)}")
     
